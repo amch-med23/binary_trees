a8ccd8b85900ab802fe3e7ca4e619839347f938e
amch-med23 <skyied.med2023@gmail.com>
2023-11-30 22:34:07 +0100

    updating the file 1 -... to fix a typo in a variable name

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,11 +7,11 @@
  * @value:  is the value to store in the new node
  *
  * Description - If parent already has a left-child, the new node must
- *               take its place, and the old left-child must be set as
- *               the left-child of the new node.
+ *				take its place, and the old left-child must be set as
+ *				the left-child of the new node.
  *
  * Return: a pointer to the created node, or NULL on failure or if
- *                      parent is NULL
+ *			parent is NULL
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
@@ -19,22 +19,18 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *node;
 
 	if (!parent)
-	{
 		return (NULL);
-	}
+
 	node = malloc(sizeof(binary_tree_t));
 	if (!node)
-	{
 		return (NULL);
-	}
+
 	node->n = value;
 	node->parent = parent;
 	node->right = NULL;
 	node->left = parent->left;
 	parent->left = node;
 	if (node->left)
-	{
-		node->left->parent = new;
-	}
+		node->left->parent = node;
 	return (node);
 }
